bc699dbe21ac00e5645d555c1c7d4a9a62cbd12d
Masanori Kaise <evisgenes.no2@gmail.com>
2020-11-03 13:30:38 +0900

    Move 01_01.cpp and 01_02.cpp to Question01.cpp

---
+++ Question01.cpp
@@ -0,0 +1,20 @@
+// Question 01_01
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Marc Kaise." << std::endl;
+//    return 0;
+//}
+
+
+// Question 01_02
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+    std::cout << str << std::endl;
+}
